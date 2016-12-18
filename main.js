f3d77c702afbe50de7187eae5b91e74adf43192f
Brian Adriance <adriab00100@gmail.com>
2016-12-17 22:21:58 -0500

    Using pug for a layout/template engine

---
+++ main.js
@@ -0,0 +1,21 @@
+var express = require('express')
+  , logger = require('morgan')
+  , app = express()
+  , template = require('pug').compileFile(__dirname + '/src/views/index.pug')
+  , appPort = 8081
+
+app.use(logger('dev'))
+app.use(express.static(__dirname + '/static'))
+
+app.get('/', function (req, res, next) {
+  try {
+    var html = template({ title: 'Home' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.listen(appPort, function () {
+  console.log('Listening on http://localhost:' + (appPort))
+})
---
+++ readme.md
@@ -0,0 +1,4 @@
+Circles
+=======
+
+A silly recursive sort of toy written in nodejs.
