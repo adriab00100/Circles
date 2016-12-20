a8fbe525db218e93392cdf14b9b82a8d91f91b42
Brian Adriance <adriab00100@gmail.com>
2016-12-19 23:11:59 -0500

    added a circle class. Added click handler on canvas to draw circle of random r

--- src/scripts/circles.js
+++ src/scripts/circles.js
@@ -1,17 +1,32 @@
 
-function setupCanvasContext() {
-	var ctx = $('#mainCanvas')[0].getContext('2d');
-	ctx.save();
-	ctx.clearRect(0,0,150,150);
-	ctx.translate(75,75);
-	ctx.scale(0.4,0.4);
-	ctx.beginPath();
-	ctx.lineWidth = 14;
-	ctx.strokeStyle = '#325FA2';
-	ctx.arc(0,0,142,0,Math.PI*2,true);
-	ctx.stroke();
+function Circle(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.draw = function (ctx) {
+        ctx.save();
+        ctx.beginPath();
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = '#325FA2';
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+        ctx.stroke();
+        ctx.restore();
+    }
 }
 
+function makeCircle(e) {
+	var x = e.clientX - $('#mainCanvas').position().left;
+	var y = e.clientY - $('#mainCanvas').position().top;
+    var c = new Circle(x, y, Math.floor((Math.random() * 90) + 10));
+    c.draw($('#mainCanvas')[0].getContext('2d'));
+}
+
+function setupCanvasContext() {
+    var canvas = $('#mainCanvas')[0];
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+	$('#mainCanvas').click(makeCircle);
+}
 
 $( document ).ready(function() {
     setupCanvasContext();
