483e694cca831bda0fd4a985544bea16dd8df362
Brian Adriance <adriab00100@gmail.com>
2016-12-20 22:10:53 -0500

    Introduce basic animation loop and the first pass at a circle triplet

--- src/scripts/circle.js
+++ src/scripts/circle.js
@@ -12,3 +12,39 @@ function Circle(x, y, radius) {
         ctx.restore();
     }
 }
+
+function TripletCircle(x, y, radius, subcircleAngle = null) {
+	this.piOverTwo = Math.PI/2.0;
+	this.fivePiOverFour = 5.0*Math.PI/4.0;
+	this.sevenPiOverFour = 7.0*Math.PI/4.0;
+    this.x = x;
+    this.y = y;
+	this.subcircleAngle = subcircleAngle;
+    this.radius = radius;
+	this.subcircles = [];
+    this.draw = function (ctx) {
+        ctx.save();
+        ctx.beginPath();
+        ctx.lineWidth = this.radius/20;
+        ctx.strokeStyle = "hsl(0, 0%, " + (this.radius/4) + "%)";
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+        ctx.stroke();
+        ctx.restore();
+		if (this.radius > 10 ) {
+			if (this.subcircles.length === 0) {
+				this.subcircles.push(new TripletCircle(0.3* this.radius * Math.cos(this.piOverTwo) + this.x, -0.3 * this.radius * Math.sin(this.piOverTwo) + this.y, this.radius/5.0, this.piOverTwo));
+				this.subcircles.push(new TripletCircle(0.3* this.radius * Math.cos(this.fivePiOverFour) + this.x, -0.3 * this.radius * Math.sin(this.fivePiOverFour) + this.y, this.radius/5.0, this.fivePiOverFour));
+				this.subcircles.push(new TripletCircle(0.3* this.radius * Math.cos(this.sevenPiOverFour) + this.x, -0.3 * this.radius * Math.sin(this.sevenPiOverFour) + this.y, this.radius/5.0, this.sevenPiOverFour));
+			}
+			for(var i = 0; i < this.subcircles.length; i++) {
+				this.subcircles[i].radius = this.radius/5.0;
+				this.subcircles[i].x = 0.3 * this.radius * Math.cos(this.subcircles[i].subcircleAngle) + this.x;
+				this.subcircles[i].y = -0.3 * this.radius * Math.sin(this.subcircles[i].subcircleAngle) + this.y;
+				this.subcircles[i].draw(ctx);
+			}
+		}
+		else {
+			this.subcircles = [];
+		}
+    }
+}
--- src/scripts/drawing_context.js
+++ src/scripts/drawing_context.js
@@ -1,7 +1,19 @@
 function DrawingContext(canvas) {
 	this.canvas = canvas;
-	this.context = canvas.getContext('2d');
+	this.context = this.canvas.getContext('2d');
+		this.circle = new TripletCircle(this.canvas.width/2, this.canvas.height/2, 50);
+	this.increment = 1.0;
+	
 	this.draw = function () {
-		this.context.clearRect(0, 0, canvas.width, canvas.height);
+		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		if(this.circle.radius * 2 >= this.canvas.width || this.circle.radius * 2 >= this.canvas.height) {
+			this.increment = -1.0;
+		}
+		if(this.circle.radius <= 10) {
+			this.increment = 1.0;
+		}
+		this.circle.radius = this.circle.radius + this.increment;
+		this.circle.draw(this.context);
+		window.requestAnimationFrame(window.mainDraw);
 	}
 }
