aeae96ee20e03d0999d075872f586c0070e568d7
Brian Adriance <adriab00100@gmail.com>
2017-01-09 20:42:31 -0500

    extract target reticle

--- src/scripts/drawing_context.js
+++ src/scripts/drawing_context.js
@@ -4,7 +4,7 @@ function DrawingContext(canvas) {
 	this.context = this.canvas.getContext('2d');
 	this.circle = new TripletCircle(this.canvas.width/2, this.canvas.height/2, 50);
 	this.increment = 1.0;
-	this.translation_target = new Object();
+	this.translationTarget = new TargetReticle();
 	
 	this.draw = function () {
 		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -14,37 +14,11 @@ function DrawingContext(canvas) {
 		// todo: keypress toggle move in/out + indicator
 		// todo: track zoomed circle
 		this.circle.draw(this.context);
-		this.targetDraw();
+		this.translationTarget.draw(this.context);
 	}
 	
 	this.trackMouse = function (e) {
-		this.translation_target.x = e.clientX - this.canvas_bounding.left;
-		this.translation_target.y = e.clientY - this.canvas_bounding.top;
-	}
-	
-	this.targetDraw = function () {
-		this.context.save();
-		this.context.lineWidth = 1;
-		this.context.strokeStyle = "red";
-		this.context.beginPath();
-		this.context.moveTo(this.translation_target.x, this.translation_target.y - 10);
-        this.context.lineTo(this.translation_target.x, this.translation_target.y - 2.5);
-		this.context.stroke();
-		this.context.beginPath();
-		this.context.moveTo(this.translation_target.x, this.translation_target.y + 10);
-        this.context.lineTo(this.translation_target.x, this.translation_target.y + 2.5);
-		this.context.stroke();
-		this.context.beginPath();
-		this.context.moveTo(this.translation_target.x - 10, this.translation_target.y);
-        this.context.lineTo(this.translation_target.x - 2.5, this.translation_target.y);
-		this.context.stroke();
-		this.context.beginPath();
-		this.context.moveTo(this.translation_target.x + 10, this.translation_target.y);
-        this.context.lineTo(this.translation_target.x + 2.5, this.translation_target.y);
-		this.context.stroke();
-		this.context.beginPath();
-		this.context.arc(this.translation_target.x, this.translation_target.y, 7.5, 0, Math.PI * 2, true);
-        this.context.stroke();
-		this.context.restore();
+		this.translationTarget.x = e.clientX - this.canvas_bounding.left;
+		this.translationTarget.y = e.clientY - this.canvas_bounding.top;
 	}
 }
---
+++ src/scripts/target_reticle.js
@@ -0,0 +1,31 @@
+
+function TargetReticle() {
+	this.x = -10;
+	this.y = -10;
+	
+	this.draw = function draw(ctx) {
+		ctx.save();
+		ctx.lineWidth = 1;
+		ctx.strokeStyle = "red";
+		ctx.beginPath();
+		ctx.moveTo(this.x, this.y - 10);
+        ctx.lineTo(this.x, this.y - 2.5);
+		ctx.stroke();
+		ctx.beginPath();
+		ctx.moveTo(this.x, this.y + 10);
+        ctx.lineTo(this.x, this.y + 2.5);
+		ctx.stroke();
+		ctx.beginPath();
+		ctx.moveTo(this.x - 10, this.y);
+        ctx.lineTo(this.x - 2.5, this.y);
+		ctx.stroke();
+		ctx.beginPath();
+		ctx.moveTo(this.x + 10, this.y);
+        ctx.lineTo(this.x + 2.5, this.y);
+		ctx.stroke();
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, 7.5, 0, Math.PI * 2, true);
+        ctx.stroke();
+		ctx.restore();
+	}
+}
